c7afc9c302ba6682994b4c30fe7879521891ac2f
ll <ll@localhost>
2026-03-06 23:59:31 -0500

    Restore legacy mobile home layout without touching fullscreen modals

--- app.js
+++ app.js
@@ -32628,7 +32628,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260306124500';
+const APP_BUILD_VERSION = '20260306132000';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

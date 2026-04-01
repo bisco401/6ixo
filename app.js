492c4154cae7dfc8c2bd433b53ffeece5f585659
ll <ll@localhost>
2026-04-01 13:22:12 -0400

    Fix Arrive+ preview card layout

--- app.js
+++ app.js
@@ -47715,7 +47715,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260401125500';
+const APP_BUILD_VERSION = '20260401130500';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

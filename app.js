2af8faa07063e33a6d9bcf4aeb09d58166b11bed
ll <ll@localhost>
2026-04-02 06:46:27 -0400

    Use original Arrive+ hero artwork

--- app.js
+++ app.js
@@ -47715,7 +47715,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260401135500';
+const APP_BUILD_VERSION = '20260402064500';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

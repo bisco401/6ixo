6dfd65e30319637d15ea8a97037dc72779c74308
ll <ll@localhost>
2026-04-03 00:47:44 -0400

    Hide baked South Africa text in Arrive+ hero

--- app.js
+++ app.js
@@ -47715,7 +47715,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260403105000';
+const APP_BUILD_VERSION = '20260403110000';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

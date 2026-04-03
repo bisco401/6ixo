881cb01a3336d92fdca1bd33c6c782021b7797b0
ll <ll@localhost>
2026-04-03 00:32:02 -0400

    Refine Arrive+ image text suppression mask

--- app.js
+++ app.js
@@ -47715,7 +47715,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260403103000';
+const APP_BUILD_VERSION = '20260403104000';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

4ccd78ad062caa418bc5d8f67d0bfb2636cd5478
ll <ll@localhost>
2026-04-03 11:38:15 -0400

    Pin Arrive+ badge to top-left and increase cover

--- app.js
+++ app.js
@@ -47715,7 +47715,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260403130000';
+const APP_BUILD_VERSION = '20260403132000';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

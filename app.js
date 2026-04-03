633c9aa75cea962c87a12eeecd06734ed22adc73
ll <ll@localhost>
2026-04-02 21:20:38 -0400

    Reduce Arrive+ hero banner height

--- app.js
+++ app.js
@@ -47715,7 +47715,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260402072500';
+const APP_BUILD_VERSION = '20260402074000';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

8df6fb4b209faa83071cb623dfaf1bd8b8ce7a26
ll <ll@localhost>
2026-04-03 01:01:46 -0400

    Use crop positioning instead of Arrive+ text mask

--- app.js
+++ app.js
@@ -47715,7 +47715,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260403111500';
+const APP_BUILD_VERSION = '20260403113000';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

f774e2ab7a3f1d713b75ce2788379eb56a3770f6
ll <ll@localhost>
2026-05-10 10:43:03 -0400

    Force visible category badges on marketplace feed images

--- app.js
+++ app.js
@@ -49085,7 +49085,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260510113000';
+const APP_BUILD_VERSION = '20260510114500';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

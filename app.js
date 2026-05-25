663ce7ee42f8be42a2533a225826da6f5d13a7bc
ll <ll@localhost>
2026-05-24 22:48:03 -0400

    Bump app build for home location search

--- app.js
+++ app.js
@@ -53345,7 +53345,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260525020500';
+const APP_BUILD_VERSION = '20260525021500';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

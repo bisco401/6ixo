f7da136237cb5a44b21185bc9e664e596539567d
ll <ll@localhost>
2026-05-26 23:48:52 -0400

    Normalize mobile category chip colors

--- app.js
+++ app.js
@@ -53758,7 +53758,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260526183500';
+const APP_BUILD_VERSION = '20260526184500';
 
 async function refreshClientForNewBuild() {
     const buildKey = 'sixo_app_build_version';

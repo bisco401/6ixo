f7f0b027123cf9b72255e4d6caece3ef27890ac8
ll <ll@localhost>
2026-06-01 21:55:22 -0400

    Update preview password

--- coming-soon-config.js
+++ coming-soon-config.js
@@ -1,6 +1,6 @@
 window.SIXO_COMING_SOON_CONFIG = {
     enabled: true,
-    passwordHash: '8a3f97489f76705783cb91b839122d1e36d6ec663a59a49549ad0ea3ef61ad58',
+    passwordHash: '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92',
     previewTtlDays: 30,
     publicOpenValues: [
         'signup',

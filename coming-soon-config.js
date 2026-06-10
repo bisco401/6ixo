bc4443fe24ed158d28a91acb4c776f6a4b950bf2
ll <ll@localhost>
2026-06-10 01:27:13 -0400

    Open public viewing

--- coming-soon-config.js
+++ coming-soon-config.js
@@ -1,5 +1,5 @@
 window.SIXO_COMING_SOON_CONFIG = {
-    enabled: true,
+    enabled: false,
     passwordHash: '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92',
     previewTtlDays: 30,
     publicOpenValues: [

565d60c0c2c487f9b57b01eb250e0b5ba2682916
ll <ll@localhost>
2026-07-12 16:35:19 -0400

    Remove vehicle city shortcut strip

--- app.js
+++ app.js
@@ -55782,7 +55782,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260712165000';
+const APP_BUILD_VERSION = '20260712165200';
 
 const SIXO_COMING_SOON_DEFAULTS = Object.freeze({
     enabled: false,

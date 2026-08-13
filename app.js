f755d88fa979372423c74c22fddff532dc73b000
ll <ll@localhost>
2026-08-13 01:04:22 -0400

    Widen mobile listing thumbnails

--- app.js
+++ app.js
@@ -60328,7 +60328,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260813150000';
+const APP_BUILD_VERSION = '20260813160000';
 
 const SIXO_COMING_SOON_DEFAULTS = Object.freeze({
     enabled: false,

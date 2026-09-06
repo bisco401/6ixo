5346afe9313d48ec0bbb1c2f22939f8d19c870bc
Ll <ll@Claudias-iMac.local>
2026-09-06 00:30:55 -0400

    Keep vehicle location labels clear of carousel dots

--- app.js
+++ app.js
@@ -63348,7 +63348,7 @@ class DatingApp {
 }
 
 // Initialize the app when the page loads
-const APP_BUILD_VERSION = '20260906041559';
+const APP_BUILD_VERSION = '20260906043041';
 
 const SIXO_COMING_SOON_DEFAULTS = Object.freeze({
     enabled: false,
